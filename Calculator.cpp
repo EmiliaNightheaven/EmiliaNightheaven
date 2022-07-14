ba914ef08675676bb10d302fe4a4338253792a32
EmiliaNightheaven <109165334+EmiliaNightheaven@users.noreply.github.com>
2022-07-14 20:15:06 +0300

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	int a,b;
+	cout << "Vvedite a " ;
+    
+    cin >> a;
+     
+    cout << "Vvedite b " ;
+      
+    cin >> b;
+    
+    cout << "a+b = " << a+b << "\n"<< "a-b = " << a-b << "\n"<< "a*b = "<< a*b << "\n"<< "a/b = " <<  a/b<< "\n"<< "a%b = " << a%b<< endl;
+}
