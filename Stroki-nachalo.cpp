68798a69bbf07316f69d9582cb284333ff6ab2bf
EmiliaNightheaven <109165334+EmiliaNightheaven@users.noreply.github.com>
2022-07-17 01:00:02 +0300

    Add files via upload

---
+++ Stroki-nachalo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+main()
+{
+	string name;
+	cout <<"Vvedite vashe imya ";
+	getline(cin, name);
+	cout << "Vashe imya is " << name << endl;
+}
